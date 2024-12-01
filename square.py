a820f385ffaadbeda4ff10fc41a27cc5c15aa587
Rajath B A <vikyath9611@gmail.com>
2024-12-01 11:14:40 +0530

    CI

--- square.py
+++ square.py
@@ -1,2 +1,12 @@
 def square_num(a):
     return a*a
+
+def prime(a):
+    if a>1:
+        for i in range(2,(a//2)+1):
+            if i%2==0:
+                return "No"
+        else:
+            return "Yes"
+    else:
+        return "No"
--- test_square.py
+++ test_square.py
@@ -1,6 +1,11 @@
-from square import square_num
+from square import square_num,prime
 
 def test_square_num():
     a = 3
     res = square_num(a)
     assert res == 9
+
+def test_prime():
+    a = 4
+    res = prime(a)
+    assert res == "No"
